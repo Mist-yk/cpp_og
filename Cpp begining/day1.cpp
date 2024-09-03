d4ed1ebd8ebe4d515ce503eedf361704d7e9f4b7
Ajay Negi <ajnegi456@gmail.com>
2024-09-03 14:25:01 +0000

    day1

---
+++ Cpp begining/day1.cpp
@@ -0,0 +1,27 @@
+// #include <iostream>
+// using namespace std;
+
+// int main (){
+//     cout << " Hello user :-"<<endl;
+// }
+
+//sum of two numbers
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int a, b;
+//     cin>>a;
+//     cin>>b;
+//     int sum = a + b;
+//     cout<<"sum is :"<<sum<<endl;
+//     return 0;
+// }
+
+//unsigned int(positive int only)
+ #include <iostream>
+ using namespace std;
+ int main(){
+ unsigned int a =-122;
+
+ cout<< a <<endl;
+}
