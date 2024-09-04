f7d6ef6a2d6459d814e6b5e80af8467c90012e9e
Ajay Negi <ajnegi456@gmail.com>
2024-09-04 13:52:03 +0000

    patterns

---
+++ Cpp complete/patterns.cpp
@@ -0,0 +1,38 @@
+//Patterns
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int m,n;
+//     cin>>m;//rows
+//     cin>>n;//column
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=m){
+//             cout << "*";
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int m,n;
+//     cin>>m;//rows
+//     cin>>n;//column
+//     int i=1;
+//     while(i<=n){
+//         int j=1;
+//         while(j<=m){
+//             cout << i;
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+// }
