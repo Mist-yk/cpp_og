99d170d17089b6ee5d9f62809c8f71020a8a400c
Ajay Negi <ajnegi456@gmail.com>
2024-09-11 19:35:20 +0530

    Update function.cpp

--- Cpp complete/function.cpp
+++ Cpp complete/function.cpp
@@ -56,6 +56,23 @@
 //     return 0;
 // }
 
+//Pass by reference 
+// #include <iostream>
+// using namespace std;
+
+// void doSomething(int  &num){
+//     cout << num <<endl;
+//     num+=5;
+//     cout << num <<endl;
+//     num +=5;
+//     cout << num <<endl;
+// }
+// int main(){
+//     int num =10;
+//     doSomething(num);
+//     cout << num <<endl;
+//     return 0;
+// }
 
 //number of set bits in a&b
 // #include <iostream>
