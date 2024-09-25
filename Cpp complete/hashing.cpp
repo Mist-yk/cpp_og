4701c9ef097a94ef124c7ef517fda2b8b3dd7988
Ajay Negi <ajnegi456@gmail.com>
2024-09-25 17:08:50 +0530

    Update hashing.cpp

--- Cpp complete/hashing.cpp
+++ Cpp complete/hashing.cpp
@@ -43,24 +43,72 @@ int main(){
 //     cout << hash[c-'a']<< endl;
 // }
 
-int n;
-cin >> n;
-int arr[n];
-for(int i=0; i<n; i++){
-    cin >> arr[i];
+// int n;
+// cin >> n;
+// int arr[n];
+// for(int i=0; i<n; i++){
+//     cin >> arr[i];
+// }
+// //precompute
+// map<int, int>mpp;
+// for(int i=0; i<n; i++){
+//     mpp[arr[i]]++;
+// }
+// int q;
+// cin >> q;
+// while(q--){
+//     int num;
+//     cin >> num;
+//     cout<<mpp[num]<<endl;
+// }
+
+//highest/lowest frequency element
+    #include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a[] = {1, 2, 1, 2, 3, 4, 3, 1}; 
+    unordered_map<int, int> mpp;
+
+    //  Count frequencies
+    for (int i = 0; i < 8; i++) {
+        mpp[a[i]]++;
+    }
+
+    // Initialize variables for max and min frequency
+    int maxFreq = INT_MIN;
+    int minFreq = INT_MAX;
+    int maxEle = -1, minEle = -1;
+
+    //  Find max and min frequency elements
+    for (auto &e : mpp) {
+        if (e.second > maxFreq) { //second is frequency
+            maxFreq = e.second; // Update maximum frequency
+            maxEle = e.first;   // Update the element with maximum frequency
+        }
+        if (e.second < minFreq) {
+            minFreq = e.second; 
+            minEle = e.first;   
+        }
+    }
+
+   
+    cout << "Element with highest frequency: " << maxEle << " (Frequency: " << maxFreq << ")\n";
+    cout << "Element with lowest frequency: " << minEle << " (Frequency: " << minFreq << ")\n";
+
+    return 0;
 }
-//precompute
-map<int, int>mpp;
-for(int i=0; i<n; i++){
-    mpp[arr[i]]++;
-}
-int q;
-cin >> q;
-while(q--){
-    int num;
-    cin >> num;
-    cout<<mpp[num]<<endl;
-}
+
+
+
+
+
+
+
+
+
+
+    
     
     return 0;
 }
