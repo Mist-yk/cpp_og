4afc7187c00f2e15c4cd5c427de24fec2ec9f262
Ajay Negi <ajnegi456@gmail.com>
2024-10-30 09:34:24 +0530

    Update advance_array.cpp

--- Cpp complete/advance_array.cpp
+++ Cpp complete/advance_array.cpp
@@ -213,3 +213,35 @@
 //     }
 //     return ans;
 // }
+
+
+
+//Longest sub array with sum k
+// #include <bits/stdc++.h>
+// using namespace std;
+
+
+// // } Driver Code Ends
+// class Solution {
+//   public:
+//     int lenOfLongestSubarr(vector<int>& arr, int k) {
+//         int left =0;
+//         int right =0;
+//         int maxLen = 0;
+//         int sum = arr[0];
+//         int max = 0;
+//         int n = arr.size();
+//         while(right < n){
+//             while(left <= right && sum > k){
+//                 sum -= arr[left];
+//                 left++;
+//             }
+//             if(sum == k){
+//                 maxLen = max(maxLen, right -left +1 );
+//             }
+//             right ++;
+//             if(right<n)sum += arr[right];
+//         }
+//         return maxLen;
+//     }
+// };
