22dc965bc47ea5406cf9223154a086e42e2e3e38
Ajay Negi <ajnegi456@gmail.com>
2024-10-31 09:03:14 +0530

    Update advance_array.cpp

--- Cpp complete/advance_array.cpp
+++ Cpp complete/advance_array.cpp
@@ -245,3 +245,25 @@
 //         return maxLen;
 //     }
 // };
+
+
+
+//MODERATE ARRAY PROBLEMS
+//two sum array
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int left =0;
+//         int right =n-1;
+//         sort(nums.begin(),nums.end());
+//         while(left < right){
+//             int sum = nums[left] + nums[right];
+//             if(sum == target) {
+//                 return "Yes";
+//             }
+//             if(sum < target) left++;
+//             else right--;
+//         }
+//         return "NO";
+//     }
+// };
